08a10602bb59eab51118ea98d0e729c698fbb598
NelloKudo <marshnelloosu@gmail.com>
2026-08-04 02:32:54 +0200

    feat: Document launcher's cmd launch options.

--- src/main.rs
+++ src/main.rs
@@ -127,9 +127,39 @@ lazy_static::lazy_static! {
     ", PROCESSED_BACKGROUND_FILE.to_string_lossy());
 }
 
+/// Actually document the launcher's command line options
+fn print_help() {
+    println!(r#"
+Honkers Launcher {APP_VERSION}
+
+Usage:
+  honkers-launcher [OPTION...]
+
+Options:
+  -h, --help            Show this help message and exit
+  --debug               Force debug output in stdout
+  --no-verbose-tracing  Disable verbose tracing output in stdout
+  --run-game            Launch the game right away if it's ready to run,
+                        otherwise open the launcher window
+  --just-run-game       Same as --run-game, but also launches the game when
+                        an update is available for predownload
+  --session <NAME>      Switch to the given session before starting
+
+GTK options are supported as well. Use --help-all to list them.
+    "#);
+}
+
 fn main() -> anyhow::Result<()> {
     // Setup custom panic handler
     human_panic::setup_panic!(human_panic::metadata!());
+
+    // Print the help message before doing anything else so that it doesn't get
+    // mixed with the tracing output, and doesn't create the launcher folders
+    if std::env::args().any(|arg| arg == "--help" || arg == "-h") {
+        print_help();
+
+        return Ok(());
+    }
 
     // Create launcher folder if it doesn't exist.
     if !LAUNCHER_FOLDER.exists() {
